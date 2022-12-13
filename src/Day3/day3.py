4d1b9c1d9180ada477afc6c830d88a0d78171a29
Shai Benjamin <sheyeh@gmail.com>
2022-12-13 17:31:11 +0200

    Day 3 part 1

--- src/Day3/day3.py
+++ src/Day3/day3.py
@@ -1,14 +1,31 @@
 priorities = 0
+group_priority = 0
 ord_a = ord('a')
 ord_A = ord('A')
+group = []
+counter = 0
+
+
+def priority(s):
+    ord_s = ord(s)
+    return ord_s - ((ord_a - 1) if ord_s > ord_a else (ord_A - 27))
+
+
 with open('day3.txt', 'r') as f:
     for line in f:
         line = line.rstrip()
+        group.append(line)
+        counter += 1
+        if counter == 3:
+            badge = [s for s in group[0] if s in group[1] and s in group[2]][0]
+            group_priority += priority(badge)
+            group = []
+            counter = 0
         L = int(len(line)/2)
         compartment_1 = line[:L]
         compartment_2 = line[L:]
-        ord_common = ord([s for s in compartment_1 if s in compartment_2][0])
-        priority = ord_common - ((ord_a - 1) if ord_common > ord_a else (ord_A - 27))
-        priorities += priority
+        common = [s for s in compartment_1 if s in compartment_2][0]
+        priorities += priority(common)
 
-print(priorities)
+print("Part 1:", priorities)
+print("Part 2:", group_priority)
