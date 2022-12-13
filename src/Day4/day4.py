e9078d6f98ec654a83a12bb6e6e8f7a3bccce333
Shai Benjamin <sheyeh@gmail.com>
2022-12-13 23:45:58 +0200

    Day 4 part 2

--- src/Day4/day4.py
+++ src/Day4/day4.py
@@ -5,10 +5,18 @@ def fully_contained(a, b, c, d):
     return (int(a) <= int(c) and int(d) <= int(b)) or (int(c) <= int(a) and int(b) <= int(d))
 
 
+def overlap(a, b, c, d):
+    return not (int(b) < int(c) or int(d) < int(a))
+
+
 contained = 0
+overlaps = 0
 with open('day4.txt', 'r') as f:
     for line in f:
         a, b, c, d = re.split("[,-]", line.rstrip())
         contained += 1 if fully_contained(a, b, c, d) else 0
+        overlaps += 1 if overlap(a, b, c, d) else 0
 
-print(contained)
+
+print("Part 1:", contained)
+print("Part 2:", overlaps)
