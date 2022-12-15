b245aa31807a557896f5d5c7775d461bcc6dcd6e
Shai Benjamin <sheyeh@gmail.com>
2022-12-15 18:51:56 +0200

    Day 8 part 2

--- src/Day8/day8.py
+++ src/Day8/day8.py
@@ -18,5 +18,22 @@ def visible(i, j):
         else 0
 
 
+def seen(trees):
+    length = len(trees)
+    for i in range(1, length):
+        if trees[i] >= trees[0]:
+            return i
+    return length - 1
+
+
+def scenic_score(i, j):
+    return seen([forrest[i][k] for k in range(j, N)]) * \
+           seen([forrest[i][k] for k in range(j, -1, -1)]) * \
+           seen([forrest[k][j] for k in range(i, N)]) * \
+           seen([forrest[k][j] for k in range(i, -1, -1)])
+
+
 count = sum(visible(i, j) for i in range(N) for j in range(N))
-print(count)
+best_scenic_score = max(scenic_score(i, j) for i in range(N) for j in range(N))
+print("Part 1:", count)
+print("Part 2:", best_scenic_score)
