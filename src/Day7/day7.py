c6bc0ea1481bd997a62aceff125f759bb3ccec46
Shai Benjamin <sheyeh@gmail.com>
2022-12-15 09:30:46 +0200

    Day 7 part 1

---
+++ src/Day7/day7.py
@@ -0,0 +1,37 @@
+import re
+
+from files import File, Directory
+
+FILE_MATCH = re.compile("(\\d*) (.*)")
+path: list[Directory] = []
+directories = []
+directory: [None, Directory] = None
+with open('day7.txt', 'r') as f:
+    for line in f:
+        line = line.rstrip()
+        if line == "$ cd ..":
+            path.pop()
+            directory = path[len(path) - 1]
+        elif line.startswith("$ cd "):
+            name = line[5:]
+            _dir = Directory(name)
+            path.append(_dir)
+            directories.append(_dir)
+            if directory:
+                directory.addSubdirectory(_dir)
+            directory = _dir
+        elif line.startswith("$ ls"):
+            continue
+        elif line.startswith("dir "):
+            continue
+        else:
+            p = FILE_MATCH.match(line)
+            file_size = int(p.group(1))
+            file_name = p.group(2)
+            file = File(file_name, file_size)
+            directory.addFile(file)
+
+
+sizes = [directory.size() for directory in directories]
+result = sum(size for size in sizes if size < 100000)
+print(result)
