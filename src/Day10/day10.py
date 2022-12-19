706c5e24b35d44b6d8444c723f89509b457d1268
Shai Benjamin <sheyeh@gmail.com>
2022-12-19 20:23:26 +0200

    Day 10 part 1

--- src/Day10/day10.py
+++ src/Day10/day10.py
@@ -3,10 +3,12 @@ x = 1
 power = 0
 check_cycle = 20
 CYCLE_DELTA = 40
+screen = ""
 
 
 def increase_cycle():
-    global cycle, check_cycle, power
+    global cycle, check_cycle, power, screen
+    screen += "#" if abs(x - cycle % 40) <= 1 else "."
     cycle += 1
     if cycle == check_cycle:
         power += cycle * x
@@ -29,4 +31,7 @@ with open('day10.txt', 'r') as f:
             delta_x = int(line.split(" ")[1])
             increase_x(delta_x)
 
-print(power)
+print("Part 1:", power)
+print("Part 2:")
+for i in range(6):
+    print(screen[i*40:(i+1)*40])
